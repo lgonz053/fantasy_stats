759fc4ba85422f68f0f453767d9eb0c85d60fc1f
Leo Gonzalez <lgonz053@gmail.com>
2019-08-12 18:41:51 -0500

    installing selenium

--- fantasy_sports/fantasy_basketball.py
+++ fantasy_sports/fantasy_basketball.py
@@ -1,5 +1,19 @@
+import os
 import requests
+import time
 from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(executable_path='C:/path/to/chromedriver.exe')
+driver.get('https://www.espn.com/nba/stats/player/_/table/offensive/sort/avgPoints/dir/desc')
+
+def load_more():
+    load_more_bttn = driver.find_element_by_class_name('loadMore__link')
+    time.sleep(2)
+    load_more_bttn.click()
+    time.sleep(2)
+
 
 def get_html():
     url = 'https://www.espn.com/nba/stats/player/_/table/offensive/sort/avgPoints/dir/desc'
@@ -12,5 +26,6 @@ def get_names():
     player_tag = espn_html.select('div > a')
     for names in player_tag:
         names = names.get_text()
+        print(names)
 
-get_names()
+load_more()
