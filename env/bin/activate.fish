5f0c7955da26caef279ff1cb57542d638ee24dbb
lgonz053 <lgonz053@gmail.com>
2019-09-21 18:04:16 -0500

    driver opens and loads more

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/leo/src/fantasy_stats/env"
+set -gx VIRTUAL_ENV "/Users/leonardogonzalez/src/fantasy_stats/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
